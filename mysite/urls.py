2316de837f60664067be9a1a61fa69152bcf281b
Geethika Gandarla <geethika.gandarla@SLS.ads.valuelabs.net>
2020-03-05 15:36:23 +0530

    first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
+    path('api-auth/', include('rest_framework.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('api/polls/', include('polls.urls')),
+    
 ]
